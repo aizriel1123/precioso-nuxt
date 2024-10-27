f0a64ef5bac992c62b8c8c9dacf104ee43066d1b
SnoreRax <davidsian0918@gmail.com>
2024-10-28 06:57:12 +0800

    smol change

--- server/api/client/client.get.ts
+++ server/api/client/client.get.ts
@@ -1,5 +1,19 @@
 import prisma from "~/lib/prisma";
 
 export default defineEventHandler(async (event) => {
-    
+    try {
+        // Fetch clients from Client Table
+        const client = await prisma.client.findMany({
+          include: {
+            Gender: true,
+          }
+        });
+        
+        // Return the data as a JSON response
+        
+        return client;
+      } catch (error) {
+        // Handle any errors that occur during the database query
+        return { error: 'Failed to fetch clients' };
+      }
 });
